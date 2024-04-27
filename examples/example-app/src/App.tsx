be97a4de567c98a249f9e322bf9dde682c37b179
EskiMojo14 <ben.j.durrant@gmail.com>
2024-04-27 02:32:32 +0100

    add example

---
+++ examples/example-app/src/App.tsx
@@ -0,0 +1,50 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { createSlice } from "@reduxjs/toolkit";
+import { useSlice } from "use-rtk-slice";
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: 0,
+  reducers: {
+    increment: (state) => state + 1,
+    decrement: (state) => state - 1,
+  },
+  selectors: {
+    selectCount: (state) => state,
+  },
+});
+
+function App() {
+  const [selectors, dispatch] = useSlice(counterSlice);
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <p>
+          <code>count</code> is {selectors.selectCount()}
+        </p>
+        <button onClick={() => dispatch.increment()}>increment</button>
+        <button onClick={() => dispatch.decrement()}>decrement</button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
