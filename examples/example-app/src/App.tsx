8d4a0b9532b7f982b2ff29e5efe6f8504d08084d
Ben Durrant <ben.durrant@marketdojo.com>
2024-04-29 18:01:54 +0100

    clean up example

--- examples/example-app/src/App.tsx
+++ examples/example-app/src/App.tsx
@@ -1,5 +1,3 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { createSlice } from "@reduxjs/toolkit";
 import { useSlice } from "use-rtk-slice";
@@ -21,28 +19,13 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <p>
           <code>count</code> is {selectors.selectCount()}
         </p>
         <button onClick={() => dispatch.increment()}>increment</button>
         <button onClick={() => dispatch.decrement()}>decrement</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
